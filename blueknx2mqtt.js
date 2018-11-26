8cd9ac69b73256e856b037df0e7e411fa64ed666
stefaan <dev@bluehome.biz>
2018-11-26 10:07:32 +0100

    building response

--- blueknx2mqtt.js
+++ blueknx2mqtt.js
@@ -19,40 +19,48 @@ const logger = createLogger({
 // Parse the ETS export
 const map = require('./etsimport.js').parse(config.knx.etsExport, logger);
 
-let handleKNXEvent = function(evt, dst, value) {
+let handleKNXEvent = function(evt, src, dst, value) {
     logger.silly("onKnxEvent %s, %s, %j", evt, dst, value);
     if (evt !== 'GroupValue_Write' && evt !== 'GroupValue_Response') {
         return;
     }
-
-    let isResponse = evt === 'GroupValue_Response';
-    let mqttMessage = value;
-    if (messageType === c.MESSAGE_TYPE_VALUE_ONLY) {
-        mqttMessage = !Buffer.isBuffer(value) ? "" + value : value
-    } else if (messageType === c.MESSAGE_TYPE_FULL) {
-        let mqttObject = {
-            value: !Buffer.isBuffer(value) ? "" + value : value
-        }
-        if (gad !== undefined) {
-            mqttObject.name = gad.name;
-            mqttObject.unit = gad.unit;
-        }
-        if (isResponse) {
-            mqttObject.response = true;
-        }
-        mqttMessage = JSON.stringify(mqttObject);
-    } else {
-        logger.error('Configured message type unknown. This should never happen and indicates a bug in the software.');
-        return;
-    }
-
+    let payload = {
+        'srcphy': src,
+        'dstgad': dst
+    };
+    enrichPayload(payload, value);
+    console.log(payload);
+    if (evt === 'GroupValue_Response') payload.response = true;
+    let mqttMessage = JSON.stringify(payload);
     logger.verbose("%s **** KNX EVENT: %s, dst: %s, value: %j",
       new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
       evt, dst, mqttMessage);
 
 }
 
-/*
+let enrichPayload = function(payload, apdu) {
+    if (map.GAToname.size === 0) return;
+    let info = map.GAToname.get(payload.dstgad);
+    console.log(info);
+    if (info === undefined) {
+        payload.value = ""; // todo: store hex value
+        payload.raw = true; // indication that payload is raw binary value
+    } else {
+        // assigns name and dpt
+        payload.dpt = info.dpt;
+        payload.main = info.main;
+        payload.middle = info.middle;
+        payload.sub = info.sub;
+        var dpt = DPTLib.resolve(info.dpt);
+        if (dpt.subtype) {
+            payload.unit = dpt.subtype.unit;
+        }
+        payload.value = DPTLib.fromBuffer(apdu, dpt);
+    }
+    // TODO: add error catches that will store the data as HEX and puts raw flag
+}
+
+
 let knxConnection = knx.Connection(Object.assign({
     handlers: {
         connected: function() {
@@ -60,13 +68,6 @@ let knxConnection = knx.Connection(Object.assign({
         },
         event: function (evt, src, dst, value) {
             logger.silly("onKnxEvent %s, %s, %j", evt, dst, value);
-            var dpt = DPTLib.resolve("9.001");
-            var payload = {};
-            if (dpt.subtype) {
-                payload.unit = dpt.subtype.unit;
-            }
-            payload.value = DPTLib.fromBuffer(value, dpt);
-            logger.info(payload.value);
+            handleKNXEvent(evt, src, dst, value);
         }
   }}, config.knx.options))
-  */
